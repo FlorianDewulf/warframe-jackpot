b6aff840b6aa2410ca5d64fb665501fdc92829ee
Florian Dewulf <florian.dewulf@epitech.eu>
2017-02-25 18:15:14 +0100

    Minify cookies and code

--- script.js
+++ script.js
@@ -45,8 +45,6 @@
       /* end of data */
     },
     mounted: function() {
-      console.log(document.cookies);
-
       var cached_data_lund = {
         warframes:  getCookie('Lund_warframes'),
         primary:    getCookie('Lund_primary'),
@@ -63,7 +61,11 @@
 
       for (var index in cached_data_lund) {
         if (typeof cached_data_lund[index] !== 'undefined' && cached_data_lund[index].length != 0) {
-          this.lund[index] = JSON.parse(cached_data_lund[index]);
+          tmp = JSON.parse(cached_data_lund[index]);
+
+          for (var i = 0 ; i < tmp.length ; ++i) {
+            this.lund[index].push({ origin: tmp[i], snake: _.snakeCase(tmp[i]) });
+          }
         }
       }
       for (var index in cached_data_ystaroth) {
@@ -114,54 +116,27 @@
       },
       restoreTextarea: function() {
         $('textarea').val('');
+
         var list = "";
+        var options = ['warframes', 'primary', 'secondary', 'melee'];
         var i = 0;
+        var area = 0;
 
-        for (var index in this.current_user.warframes) {
-          if (i != 0) {
-            list += "\n";
-          }
-          $('body').append('<img src="assets/warframes/' + this.current_user.warframes[index].sn + '.png" />');
-          list += this.current_user.warframes[index].orig;
-          ++i;
-        }
-        $($('textarea')[0]).val(list);
-        list = '';
+        for (var name_option in options) {
+          i = 0;
+          list = '';
 
-        i = 0;
-        for (var index in this.current_user.primary) {
-          if (i != 0) {
-            list += "\n";
+          for (var index in this.current_user[name_option]) {
+            if (i != 0) {
+              list += "\n";
+            }
+            $('body').append('<img src="assets/' + name_option + '/' + this.current_user[name_option][index].snake + '.png" />');
+            list += this.current_user[name_option][index].origin;
+            ++i;
           }
-          $('body').append('<img src="assets/primary/' + this.current_user.primary[index].sn + '.png" />');
-          list += this.current_user.primary[index].orig;
-          ++i;
+          $($('textarea')[area]).val(list);
+          ++area;
         }
-        $($('textarea')[1]).val(list);
-        list = '';
-
-        i = 0;
-        for (var index in this.current_user.secondary) {
-          if (i != 0) {
-            list += "\n";
-          }
-          $('body').append('<img src="assets/secondary/' + this.current_user.secondary[index].sn + '.png" />');
-          list += this.current_user.secondary[index].orig;
-          ++i;
-        }
-        $($('textarea')[2]).val(list);
-        list = '';
-
-        i = 0;
-        for (var index in this.current_user.melee) {
-          if (i != 0) {
-            list += "\n";
-          }
-          $('body').append('<img src="assets/melee/' + this.current_user.melee[index].sn + '.png" />');
-          list += this.current_user.melee[index].orig;
-          ++i;
-        }
-        $($('textarea')[3]).val(list);
       },
       roll: function(event) {
         var nums = this.getNumsFromRange({
@@ -182,8 +157,8 @@
         			elm.toggleClass('spin');
 
       				setTimeout(function () {
-                if (typeof that.current_user[type][nums[type]] !== 'undefined' && that.current_user[type][nums[type]].orig != '') {
-        					elm.html('<img src="assets/' + type + "/" + that.current_user[type][nums[type]].sn + '.png" /><br />' + that.current_user[type][nums[type]].orig);
+                if (typeof that.current_user[type][nums[type]] !== 'undefined' && that.current_user[type][nums[type]].origin != '') {
+        					elm.html('<img src="assets/' + type + "/" + that.current_user[type][nums[type]].snake + '.png" /><br />' + that.current_user[type][nums[type]].origin);
                 } else {
                   elm.html('?');
                 }
@@ -196,51 +171,55 @@
       },
       putWarframes: function(event) {
         var final = [];
+        var light_for_cookie = [];
         var values = this.parse_textarea($(event.target).val());
-        var orig = this.parse_textarea($(event.target).val());
+        var origin = this.parse_textarea($(event.target).val());
 
         for (var index in values) {
-          final.push({orig: orig[index], sn: _.snakeCase(values[index]) });
+          final.push({origin: origin[index], snake: _.snakeCase(values[index]) });
+          light_for_cookie.push(origin[index]);
         }
         this.current_user.warframes = final;
-        setCookie(this.current_user.name + '_warframes', JSON.stringify(this.current_user.warframes));
-        console.log(document.cookies);
+        setCookie(this.current_user.name + '_warframes', JSON.stringify(light_for_cookie));
       },
       putPrimaries: function(event) {
         var final = [];
+        var light_for_cookie = [];
         var values = this.parse_textarea($(event.target).val());
-        var orig = this.parse_textarea($(event.target).val());
+        var origin = this.parse_textarea($(event.target).val());
 
         for (var index in values) {
-          final.push({orig: orig[index], sn: _.snakeCase(values[index]) });
+          final.push({origin: origin[index], snake: _.snakeCase(values[index]) });
+          light_for_cookie.push(origin[index]);
         }
         this.current_user.primary = final;
-        setCookie(this.current_user.name + '_primary', JSON.stringify(this.current_user.primary));
-        console.log(document.cookies);
+        setCookie(this.current_user.name + '_primary', JSON.stringify(light_for_cookie));
       },
       putSecondaries: function(event) {
         var final = [];
+        var light_for_cookie = [];
         var values = this.parse_textarea($(event.target).val());
-        var orig = this.parse_textarea($(event.target).val());
+        var origin = this.parse_textarea($(event.target).val());
 
         for (var index in values) {
-          final.push({orig: orig[index], sn: _.snakeCase(values[index]) });
+          final.push({origin: origin[index], snake: _.snakeCase(values[index]) });
+          light_for_cookie.push(origin[index]);
         }
         this.current_user.secondary = final;
-        setCookie(this.current_user.name + '_secondary', JSON.stringify(this.current_user.secondary));
-        console.log(document.cookies);
+        setCookie(this.current_user.name + '_secondary', JSON.stringify(light_for_cookie));
       },
       putMelees: function(event) {
         var final = [];
+        var light_for_cookie = [];
         var values = this.parse_textarea($(event.target).val());
-        var orig = this.parse_textarea($(event.target).val());
+        var origin = this.parse_textarea($(event.target).val());
 
         for (var index in values) {
-          final.push({orig: orig[index], sn: _.snakeCase(values[index]) });
+          final.push({origin: origin[index], snake: _.snakeCase(values[index]) });
+          light_for_cookie.push(origin[index]);
         }
         this.current_user.melee = final;
-        setCookie(this.current_user.name + '_melee', JSON.stringify(this.current_user.melee));
-        console.log(document.cookies);
+        setCookie(this.current_user.name + '_melee', JSON.stringify(light_for_cookie));
       },
       parse_textarea: function(content) {
         return content.split("\n");
